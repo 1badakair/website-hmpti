45329b87a2d232dcbf5921f964044f75ea171166
Dhika Primanda <dhikaprimanda.dp@gmail.com>
2026-07-06 18:31:56 +0700

    fix: fix profile picture

--- data/profile.ts
+++ data/profile.ts
@@ -1,7 +1,5 @@
 import type { Department, ProfileDepartment, ProfileMember } from "@/types"
 
-const memberImage = "/figma/xavier.png"
-
 type ProfileMemberWithUrl = ProfileMember & {
   profileUrl?: string
 }
@@ -11,35 +9,35 @@ const bphMembers: ProfileMemberWithUrl[] = [
     id: "bph-1",
     name: "Xavier",
     role: "Ketua",
-    image: "/figma/xavier.png",
+    image: "/assets/profile/xavier.png",
     profileUrl: "https://xavierwijayahere.vercel.app/",
   },
   {
     id: "bph-2",
     name: "Berlian",
     role: "Sekretaris I",
-    image: memberImage,
+    image: "/assets/profile/berlian.png",
     profileUrl: "https://linktr.ee/berlianrzn?utm_source=qr_code",
   },
   {
     id: "bph-3",
     name: "Jeremy",
     role: "Sekretaris II",
-    image: memberImage,
+    image: "/assets/profile/jeremy.png",
     profileUrl: "https://portfolio-jeremy-trystan-amber.vercel.app/",
   },
   {
     id: "bph-4",
     name: "Audel",
     role: "Bendahara I",
-    image: memberImage,
+    image: "/assets/profile/audel.png",
     profileUrl: "https://linktr.ee/audeliatjh",
   },
   {
     id: "bph-5",
     name: "Wynona",
     role: "Bendahara II",
-    image: memberImage,
+    image: "/assets/profile/wynona.png",
     profileUrl: "https://linktr.ee/wynona?utm_source=qr_code",
   },
 ]
@@ -49,28 +47,28 @@ const operationMembers: ProfileMemberWithUrl[] = [
     id: "operation-1",
     name: "Adit",
     role: "Coordinator",
-    image: memberImage,
+    image: "/assets/profile/adit.png",
     profileUrl: "https://linktr.ee/twahyuaditya",
   },
   {
     id: "operation-2",
     name: "Yohanes",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/yohanes.png",
     profileUrl: "https://yohaneswp.vercel.app/",
   },
   {
     id: "operation-3",
     name: "Yosua",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/yosua.png",
     profileUrl: "https://linktr.ee/672024016?utm_source=qr_code",
   },
   {
     id: "operation-4",
     name: "Dhika",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/dhika.png",
     profileUrl: "https://linktr.ee/dhikaprimanda",
   },
 ]
@@ -80,20 +78,20 @@ const creativeMembers: ProfileMemberWithUrl[] = [
     id: "creative-1",
     name: "Theo",
     role: "Coordinator",
-    image: memberImage,
+    image: "/assets/profile/theo.png",
     profileUrl: "https://www.instagram.com/aratakitheo/",
   },
   {
     id: "creative-2",
     name: "Samuel",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/samuel.png",
   },
   {
     id: "creative-3",
     name: "Khulud",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/khulud.png",
     profileUrl:
       "https://linktr.ee/Khulud_zlyn?utm_source=linktree_profile_share&ltsid=26bf897c-82c8-4d13-b420-c2ad84feee7f",
   },
@@ -101,7 +99,7 @@ const creativeMembers: ProfileMemberWithUrl[] = [
     id: "creative-4",
     name: "Agri",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/agri.png",
   },
 ]
 
@@ -110,28 +108,28 @@ const academicMembers: ProfileMemberWithUrl[] = [
     id: "academic-1",
     name: "Matheo",
     role: "Coordinator",
-    image: memberImage,
+    image: "/assets/profile/matheo.png",
     profileUrl: "https://linktr.ee/matheoamarya?utm_source=qr_code",
   },
   {
     id: "academic-2",
     name: "Zia",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/zia.png",
     profileUrl: "https://linktr.ee/fahzya",
   },
   {
     id: "academic-3",
     name: "John",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/john.png",
     profileUrl: "https://linktr.ee/johnmsp",
   },
   {
     id: "academic-4",
     name: "Widhi",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/widhi.png",
     profileUrl:
       "https://www.linkedin.com/in/widhi-kristianto-0816a2358?utm_source=share&utm_campaign=share_via&utm_content=profile&u",
   },
@@ -142,27 +140,27 @@ const humanitarianMembers: ProfileMemberWithUrl[] = [
     id: "humanitarian-1",
     name: "Trista",
     role: "Coordinator",
-    image: memberImage,
+    image: "/assets/profile/trista.png",
     profileUrl: "https://linktr.ee/candrarinitrista?utm_source=qr_code",
   },
   {
     id: "humanitarian-2",
     name: "Cesa",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/cesa.png",
     profileUrl: "https://linktr.ee/cesacaa?utm_source=qr_code",
   },
   {
     id: "humanitarian-3",
     name: "Talita",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/talita.png",
   },
   {
     id: "humanitarian-4",
     name: "Imam",
     role: "Staff",
-    image: memberImage,
+    image: "/assets/profile/imam.png",
     profileUrl:
       "https://www.linkedin.com/in/imamadiwicaksana?utm_source=share_via&utm_content=profile&utm_medium=member_android",
   },
